9fbd67971fc1bce4cd5aedd73cdbf92b4bb61647
justcallmeHao <haovo.workspace@gmail.com>
2025-07-31 18:10:49 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,30 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // For local development
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [
-//     react(),
-//     mode === 'development' &&
-//     componentTagger(),
-//   ].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
-
-// For GitHub hosting built
-export default defineConfig({
-  base: '/UQRL_AR_generation/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}));
+
+// For GitHub hosting built
+// export default defineConfig({
+//   base: '/UQRL_AR_generation/',
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
